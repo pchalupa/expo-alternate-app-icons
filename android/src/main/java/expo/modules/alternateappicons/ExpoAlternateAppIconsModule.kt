950de6225e92d533f9b5214b689c45c3d33a00d5
pchalupa <chalupa.petr93@gmail.com>
2024-05-07 17:28:26 +0200

    feat: get and set icon

--- android/src/main/java/expo/modules/alternateappicons/ExpoAlternateAppIconsModule.kt
+++ android/src/main/java/expo/modules/alternateappicons/ExpoAlternateAppIconsModule.kt
@@ -1,5 +1,7 @@
 package expo.modules.alternateappicons
 
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
@@ -17,10 +19,24 @@ class ExpoAlternateAppIconsModule : Module() {
   }
 
   private fun getAppIconName(): String? {
-    return null
+    val activityName = appContext.activityProvider?.currentActivity?.componentName?.shortClassName
+
+    if(activityName !== null && activityName.startsWith(".MainActivity")) return null
+
+
+    println("activity name is:")
+    println(activityName)
+
+
+    return activityName
   }
 
   private fun setAlternateAppIcon(icon: String?, promise: Promise): String? {
-    return null
+    appContext.reactContext?.packageManager?.setComponentEnabledSetting(ComponentName("expo.modules.alternateappicons.example", "expo.modules.alternateappicons.example.MainActivityLight"), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
+    appContext.reactContext?.packageManager?.setComponentEnabledSetting(ComponentName("expo.modules.alternateappicons.example", "expo.modules.alternateappicons.example.MainActivity"), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
+
+    promise.resolve(icon)
+
+    return icon
   }
 }
